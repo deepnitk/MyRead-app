6729c414c31e38a683deed179513a9bac9597ef1
apple <ratnadeep.konar@gmail.com>
2019-04-13 18:32:16 +0530

    create empty component

---
+++ src/BooksList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class BooksList extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default BooksList;
---
+++ src/BooksSearch.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import PropTypes from'prop-types'
+
+class BookSearch extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default BookSearch;
